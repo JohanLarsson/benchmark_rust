cf144eae8b2eda90245785cff2d3158f974d0503
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-04 17:15:36 +0200

    Add map benchmark.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@ extern crate stopwatch;
 use stopwatch::{Stopwatch};
 
 fn main() {
+	filter_and_sum(1000000, 1000);
+	filter_divide_and_sum(1000000, 1000);
+}
+
+fn filter_and_sum(n: i32, m: i32){
 	let sw = Stopwatch::start_new();
-	let sum_evens = (0..1000000)
-					.filter(|x| x % 1000 == 0)
+	let sum = (0..n).filter(|x| x % m == 0)
 					.fold(0, |sum, x| sum + x);
-	println!("{0}: {1} ms", sum_evens, sw.elapsed_ms());
+	println!("{0}: {1} ms", sum, sw.elapsed_ms());
+}
+
+fn filter_divide_and_sum(n: i32, m: i32){
+	let sw = Stopwatch::start_new();
+	let sum = (0..n).filter(|x| x % m == 0)
+					.map(|x| x as f32/3.0)
+					.fold(0.0, |sum, x| sum + x);
+	println!("{0}: {1} ms", sum, sw.elapsed_ms());
 }
