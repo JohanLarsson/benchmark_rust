c645c4d5c1644bfa3b6c24ff680e46debe94e52c
Seth Heeren <sgheeren@gmail.com>
2015-10-05 23:50:03 +0200

    Using `nom`'s benchmark utility
    Disclaimer: cargo-cult all the way

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-extern crate stopwatch;
-use stopwatch::{Stopwatch};
+#![feature(test)]
+extern crate test;
 
-fn main() {
-	filter_and_sum(1000000, 1000);
-	filter_divide_and_sum(1000000, 1000);
+use test::Bencher;
+#[bench]
+fn filter_and_sum_bench(b: &mut Bencher) {
+  b.iter(||{
+    filter_and_sum(1000000, 1000)
+  });
 }
 
-fn filter_and_sum(n: i32, m: i32){
-	let sw = Stopwatch::start_new();
+#[bench]
+fn filter_divide_and_sum_bench(b: &mut Bencher) {
+  b.iter(||{
+	filter_divide_and_sum(1000000, 1000)
+  });
+}
+
+fn filter_and_sum(n: i32, m: i32) -> i32{
 	let sum = (0..n).filter(|x| x % m == 0)
 					.fold(0, |sum, x| sum + x);
-	println!("{0}: {1} ms", sum, sw.elapsed_ms());
+    return sum;
 }
 
-fn filter_divide_and_sum(n: i32, m: i32){
-	let sw = Stopwatch::start_new();
+fn filter_divide_and_sum(n: i32, m: i32) -> f32 {
 	let sum = (0..n).filter(|x| x % m == 0)
 					.map(|x| x as f32/3.0)
 					.fold(0.0, |sum, x| sum + x);
-	println!("{0}: {1} ms", sum, sw.elapsed_ms());
+    return sum;
 }
